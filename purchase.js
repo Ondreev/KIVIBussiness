c6d38a04e93f406e2535b6ad12a514a5960ca729
Ondreev <mrtutanhamon@gmail.com>
2025-08-26 16:35:58 +0300

    Update purchase.js

--- purchase.js
+++ purchase.js
@@ -1,7 +1,7 @@
 // purchase.js — Рекомендованная сумма закупки
 
 (async () => {
-  const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTM-GTiL5auNwSsi0SWkR5_YzX89K-J27vC5nw15bVJbkJRXrmXzNv4LDWb32xfVHNcYac0GnNsxJTI/pub?gid=2099900296&single=true&output=csv";
+  const dataUrl = SHEETS.data;     // вкладка "Данные";
 
   const parse = async (url) => {
     const res = await fetch(url);
